16ca9412895e7f412340fe2c5cc26d79a2c3fd08
Anton Sander <mail@antonsander.de>
2025-12-09 10:54:34 +0100

    Day 5 Part 1 done

--- 2025/day05/day5.sh
+++ 2025/day05/day5.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
-#
 
+is_fresh(){
+  local range="$1" id="$2"
+  local start end
+
+  start=${range%-*}
+  end=${range#*-}
+
+  (( id <= end && id >= start)) && return 0
+  return 1
+}
+
+
+file="${1:-test.txt}"
 steps=0
 adding_up=0
 ranges=()
 ids=()
 block=1
+fresh=0
 
 while read -r line; do
   if [[ -z $line ]]; then
-    ((block++))
+    ((block++)) && continue
   fi
   if (( block == 1 )); then
     ranges+=("$line")
   else
     ids+=("$line")
   fi
-done < test.txt
+done < $file
 
-for range in "${ranges[@]}"; do
+for id in "${ids[@]}"; do
   ((steps++))
 
-  echo "$steps : $range"
-
-  start=${range%-*}
-  end=${range#*-}
-
-  for ((item=start; item<=end; item++)); do
-    echo "    $item"
+  for range in "${ranges[@]}"; do
+    echo -n "$steps : $range"
+    if is_fresh $range $id; then
+      echo " #$id: 1"
+	  ((fresh++))
+	  break
+    else
+      echo " #$id: 0"
+	fi
   done
+
 done
+
+echo
+echo "$fresh of the available ingredient IDs are fresh"
