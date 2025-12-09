28e6ea0ef0207c0d1da166ed3b7085a19d5e35e7
Anton Sander <mail@antonsander.de>
2025-12-09 01:02:32 +0100

    Day 4 Part 2 done

--- 2025/day04/day4.1.sh
+++ 2025/day04/day4.1.sh
@@ -2,7 +2,10 @@
 
 part(){
     local sum
+    local newline
+    echo -n "   "
     if [[ "$line2" != "" ]]; then
+      newline="$line2"
       for (( col = 0; col < ${#line2}; col++ )); do 
 	echo -n " #"
         sum=0
@@ -22,34 +25,52 @@ part(){
 	  [[ "$roll" = "${line2:i:1}" ]] && (( sum += weight ))
 	  [[ "$roll" = "${line3:i:1}" ]] && (( sum += weight ))
 	fi
-        (( sum < neighbors )) && (( adding_up++ ))
+        if (( sum < neighbors )); then
+	      (( adding_up++ ))
+	      newline="${newline:0:col}.${newline:col+1}"
+	fi
+	echo -n "$sum"
       done
+      newmap+=("$newline")
     fi
     line1=$line2
     line2=$line3
     line3=""
 }
 
+file="input.txt"
+map=()
 steps=0
-adding_up=0
 neighbors=4
 weight=1
 roll="@"
-line1=""
-line2=""
+sum=0
+adding_up=1
 
-echo "Day 4 Part 1"
+echo "Day 4 Part 2"
 echo
 
 while read -r -a line3; do
+  map+=("$line3")
+done < <( cat $file; echo "")
 
-      (( steps++ ))
-    
-      echo -n "$steps : $line3"
+while (( 0 != adding_up )); do
+  newmap=()
+  line1=""
+  line2=""
+  adding_up=0
+  for row in "${!map[@]}"; do
+      line3="${map[$row]}"
+      echo -n "$row : $line3"
       part 
       echo
-
-done < <( cat input.txt; echo "")
+  done
+  echo "*** $adding_up"
+  (( sum += adding_up ))
+  map=()
+  newmap+=("\n")
+  map=("${newmap[@]}")
+done
 
 echo
-echo "$adding_up rolls of paper can be accessed by a forklift"
+echo "$sum rolls of paper can be accessed by a forklift"
