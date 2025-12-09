41c3c1927d5203b3d3113a95b17af38dc013e9a3
Anton Sander <mail@antonsander.de>
2025-12-10 00:15:41 +0100

    Day 5 Part 2 done. Das war ein Kampf!

--- 2025/day05/day5.1.sh
+++ 2025/day05/day5.1.sh
@@ -1,51 +1,56 @@
 #!/bin/bash
 
-is_fresh(){
-  local range="$1" id="$2"
-  local start end
-
-  start=${range%-*}
-  end=${range#*-}
-
-  (( id <= end && id >= start)) && return 0
-  return 1
+get_ranges(){
+  while read -r line; do
+    [[ -n $line ]] && ranges+=("$line") || break
+  done < $file
 }
 
+set_ranges(){
+  local start1 end1 strt2 end2
+  local length="${#ranges[@]}"
+  for ((x = 0; x < length; x++ )); do
+    [[ "${ranges[x]}" == "" ]] && continue 
+    for ((y = x+1; y < length; y++ )); do
+      [[ "${ranges[y]}" == "" ]] && continue 
+      start1=${ranges[x]%-*} end1=${ranges[x]#*-}
+      start2=${ranges[y]%-*} end2=${ranges[y]#*-}
+      if (( start1 > start2 )); then
+	    tmp="${ranges[y]}" ranges[y]="${ranges[x]}" ranges[x]="$tmp"
+		start2="${start1}"       end2="${end1}"
+        start1="${ranges[x]%-*}" end1="${ranges[x]#*-}"
+		(( y = x ))
+		continue
+	  fi
+      (( (end1+1) < start2 )) && continue 
+	  (( (end2 > end1) )) && ranges[x]="${start1}-${end2}"
+      ranges[y]=""
+      (( y = x ))
+      continue
+    done
+  done
+}
+
+calc_fresh(){
+  local sum start end
+  for x in "${!ranges[@]}"; do
+    [[ "${ranges[x]}" == "" ]] && continue 
+    start=${ranges[x]%-*}
+    end=${ranges[x]#*-}
+    (( sum = end - start + 1 ))
+    echo "$x : ${ranges[x]} # ${sum}"
+    (( fresh += sum ))
+  done
+}
+
+###############################################################################
 
 file="${1:-test.txt}"
-steps=0
-adding_up=0
-ranges=()
-ids=()
-block=1
-fresh=0
+ranges=() fresh=0
 
-while read -r line; do
-  if [[ -z $line ]]; then
-    ((block++)) && continue
-  fi
-  if (( block == 1 )); then
-    ranges+=("$line")
-  else
-    ids+=("$line")
-  fi
-done < $file
-
-for id in "${ids[@]}"; do
-  ((steps++))
-
-  for range in "${ranges[@]}"; do
-    echo -n "$steps : $range"
-    if is_fresh $range $id; then
-      echo " #$id: 1"
-	  ((fresh++))
-	  break
-    else
-      echo " #$id: 0"
-	fi
-  done
-
-done
+get_ranges
+set_ranges
+calc_fresh
 
 echo
 echo "$fresh of the available ingredient IDs are fresh"
