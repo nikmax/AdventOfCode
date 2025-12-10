222769e873a0f1b523464bdf899a58d2cf166a1d
Anton Sander <mail@antonsander.de>
2025-12-10 23:48:44 +0100

    Day 7 Part 1 done

--- 2025/day07/day07.sh
+++ 2025/day07/day07.sh
@@ -9,23 +9,55 @@ cell=""
 entry="S"
 empty_space="."
 splitter="^"
+beam="|"
+beams=()
+show=""
 
 SECONDS=0
 
 while read -r line; do
   max="${#line}"
-  for (( x=0; x < $max; x++ )); do
+  show=""
+  for (( x=0; x <= $max; x++ )); do
     cell="${line:x:1}"
-    [[ "$cell" == "$entry" ]] && start=$x
-    echo -n "$cell"
+    case "$cell" in
+      "$entry")
+        beams[x]="${beam}"
+        ;;
+      "$empty_space")
+        [[ "${beams[x]}" == "$beam" ]] && cell="$beam"
+        ;;
+      "$splitter")
+        if [[ "${beams[x]}" == "$beam" ]]; then
+
+          beams[x]="$empty_space"
+          (( total++ ))
+          if (( x > 0 )); then
+            (( x-- ))
+            beams[x]="$beam"
+            show="${show:0:x}${beam}"
+            (( x++ ))
+          fi
+          if (( x < max -1 )); then
+            show+="$cell"
+            cell="$beam"
+            (( x++ ))
+            beams[x]="$beam"
+          #  cell+="$empty_space"   # (( x-- ))
+          fi
+           
+        fi
+        ;;
+    esac
+
+    show+="$cell"
   done
-  echo
+  echo "$show $total"
 done < $file
 
-echo "starts at position ${start} of ${max}"
-
+echo
 echo "============"
 echo "Day 7 Part 1"
 echo "============"
-echo "The beam will be spliti a $total times."
+echo "The beam will be split a $total times."
 echo "Duration: $SECONDS Seconds"
