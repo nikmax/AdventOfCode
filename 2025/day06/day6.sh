450e0340e0fda484333eaa0b18a1d26644380151
Anton Sander <mail@antonsander.de>
2025-12-10 10:15:24 +0100

    Day 6 Part 1 done :)

--- 2025/day06/day6.sh
+++ 2025/day06/day6.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
-get_worksheet(){
-  while read -r line; do
-    problems+=("$line")
-  done < $file
+calculate_problems(){
+    while read -r -a line; do
+        for x in "${!line[@]}"; do
+            if [[ ${line[x]} == "+" || ${line[x]} == "*" ]]; then
+                total_ops=("${line[@]}")
+                continue
+            fi
+            [[ -z ${total_mul[x]} ]] && (( total_mul[x] = 1 ))
+            (( total_mul[x] *= line[x] ))
+            (( total_add[x] += line[x] ))
+        done
+        echo "${line[@]}"
+        echo "${total_mul[@]}"
+        echo "${total_add[@]}"
+        echo
+    done < $file
 }
 
-show_problems(){
-  for x in "${!problems[@]}"; do
-    echo "$x : ${problems[x]}"
-  done
+calculate_total(){
+    for x in "${!total_ops[@]}"; do
+        [[ "${total_ops[x]}" == "*" ]] && (( total += total_mul[x] ))
+        [[ "${total_ops[x]}" == "+" ]] && (( total += total_add[x] ))
+        echo "${total_ops[x]} : $total"
+    done
 }
 
 ###############################################################################
 
 file="${1:-test.txt}"
-problems=()
 total=0
+total_mul=()
+total_add=()
+total_ops=()
 
-get_worksheet
-show_problems
+SECONDS=0
+
+calculate_problems
+calculate_total
 
 echo
 echo "In this worksheet, the grand total is $total"
+echo "Duration: $SECONDS Seconds"
